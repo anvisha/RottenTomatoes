53005a6977926344fc2a5875da59c77a419e8ff2
anvisha <anvisha@gmail.com>
2015-09-14 23:44:53 -0700

    first shizz

--- RottenTomatoes/ViewController.swift
+++ RottenTomatoes/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var moviesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        moviesTableView.rowHeight = 200.0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("com.example.cell") as! MyCell
+        cell.myCustomLabel.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let vc = DetailViewController(nibName: nil, bundle: nil)
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
+class MyCell : UITableViewCell {
+
+    @IBOutlet weak var myCustomLabel: UILabel!
+}
+
+class DetailViewController : UIViewController {
+    override func loadView() {
+        let v = UIView(frame: CGRectZero)
+        v.backgroundColor = UIColor.greenColor()
+        self.view = v
+    }
+}
